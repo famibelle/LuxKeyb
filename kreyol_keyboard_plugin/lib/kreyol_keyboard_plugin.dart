060952b6a567e9cea5f16c63388e514d616f2632
famibelle <medhi.famibelle@gmail.com>
2025-09-07 21:24:14 +0200

    refactor: slim repository to android_keyboard only (option A)

--- kreyol_keyboard_plugin/lib/kreyol_keyboard_plugin.dart
+++
@@ -1,8 +0,0 @@
-
-import 'kreyol_keyboard_plugin_platform_interface.dart';
-
-class KreyolKeyboardPlugin {
-  Future<String?> getPlatformVersion() {
-    return KreyolKeyboardPluginPlatform.instance.getPlatformVersion();
-  }
-}
